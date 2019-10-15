ac6c3fb015d3164bc4426f190b5854e669ce4369
Nathan Lesne <nathan.lesne@etu.u-bordeaux.fr>
2019-10-15 06:10:41 +0200

    Fixed overflow_size to actually test if GetString prints the warning

--- nachos/code/test/getstring.c
+++ nachos/code/test/getstring.c
@@ -2,7 +2,7 @@
 int
 main ()
 {
-  int overflow_size = 32;
+  int overflow_size = 256;
   char s[overflow_size];
   GetString(s,overflow_size); // value will be ignored and replaced by MAX_STRING_SIZE
   PutString(s);
